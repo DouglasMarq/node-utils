2924f4717cc07bfe34819d9bcdd9fbdb2eda48cc
DouglasMarq <douglas.marq.alves@outlook.com>
2021-01-28 07:49:41 -0300

    Stats

--- src/index.js
+++ src/index.js
@@ -6,5 +6,7 @@ global.settings = require('./settings.json');
   schedule.scheduleJob(`*/${global.settings.cpuTimerSeconds} * * * * *`, async () => {
     console.log('free CPU:: ', await watcher.getFreeCPU());
     console.log('free Mem:: ', await watcher.getFreeMem());
+    console.log('free Disk:: ', await watcher.getFreeDiskSpace());
+    console.log('Stats:: ', await watcher.getAllStats());
   });
 })()
--- src/service/watch/index.js
+++ src/service/watch/index.js
@@ -1,17 +1,41 @@
 const osu = require('node-os-utils');
-const checkDiskSpace = require('check-disk-space');
 
 let watcherStruct = {
-  cpuFree: 0,
-  freeMem: 0
+  cpu: {
+    cpuFree: 0,
+    cpuCores: 0
+  },
+  mem: {
+    freeMem: 0,
+    totalMem: 0
+  },
+  disk: {
+    freeDisk: 0,
+    freeDiskP: 0
+  }
 }
 
 exports.getFreeCPU = async () => {
-  watcherStruct.cpuFree = `${Number((await osu.cpu.free()).toFixed(2))} %`
-  return watcherStruct.cpuFree;
+  watcherStruct.cpu.cpuFree = `${(await osu.cpu.free()).toFixed(2)} %`
+  watcherStruct.cpu.cpuCores = `${osu.cpu.count()}`
+  return watcherStruct.cpu;
 }
 
 exports.getFreeMem = async () => {
-  watcherStruct.freeMem = `${Number((await osu.mem.free()).freeMemMb)} MB`
-  return watcherStruct.freeMem;
+  watcherStruct.mem.freeMem = `${(await osu.mem.free()).freeMemMb} MB`
+  watcherStruct.mem.totalMem = `${(await osu.mem.free()).totalMemMb} MB`
+  return watcherStruct.mem;
+}
+
+exports.getFreeDiskSpace = async () => {
+  watcherStruct.disk.freeDisk = `${(await osu.drive.free()).freeGb} GB`
+  watcherStruct.disk.freeDiskP = `${(await osu.drive.free()).freePercentage} %`
+  return watcherStruct.disk;
+}
+
+exports.getAllStats = async () => {
+  await this.getFreeCPU();
+  await this.getFreeMem();
+  await this.getFreeDiskSpace();
+  return watcherStruct;
 }
