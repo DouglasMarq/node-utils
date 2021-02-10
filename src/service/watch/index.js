1e7ef2aa86f63b95d351a11617fce303dc0e22b1
DouglasMarq <douglas.marq.alves@outlook.com>
2021-02-10 08:44:19 -0300

    Adicionando testes

--- src/service/watch/index.js
+++ src/service/watch/index.js
@@ -1,40 +1,40 @@
 const osu = require('node-os-utils');
-let watcherStruct = {
+const watcherStruct = {
   cpu: {
     cpuFree: 0,
-    cpuCores: 0
+    cpuCores: 0,
   },
   mem: {
     freeMem: 0,
-    totalMem: 0
+    totalMem: 0,
   },
   disk: {
     freeDisk: 0,
-    freeDiskP: 0
-  }
-}
+    freeDiskP: 0,
+  },
+};
 
 exports.getFreeCPU = async () => {
-  watcherStruct.cpu.cpuFree = `${(await osu.cpu.free()).toFixed(2)} %`
-  watcherStruct.cpu.cpuCores = `${osu.cpu.count()}`
+  watcherStruct.cpu.cpuFree = Number(`${(await osu.cpu.free()).toFixed(2)}`);
+  watcherStruct.cpu.cpuCores = Number(`${osu.cpu.count()}`);
   return watcherStruct.cpu;
-}
+};
 
 exports.getFreeMem = async () => {
-  watcherStruct.mem.freeMem = `${(await osu.mem.free()).freeMemMb} MB`
-  watcherStruct.mem.totalMem = `${(await osu.mem.free()).totalMemMb} MB`
+  watcherStruct.mem.freeMem = Number(`${(await osu.mem.free()).freeMemMb}`);
+  watcherStruct.mem.totalMem = Number(`${(await osu.mem.free()).totalMemMb}`);
   return watcherStruct.mem;
-}
+};
 
 exports.getFreeDiskSpace = async () => {
-  watcherStruct.disk.freeDisk = `${(await osu.drive.free()).freeGb} GB`
-  watcherStruct.disk.freeDiskP = `${(await osu.drive.free()).freePercentage} %`
+  watcherStruct.disk.freeDisk = Number(`${(await osu.drive.free()).freeGb}`);
+  watcherStruct.disk.freeDiskP = Number(`${(await osu.drive.free()).freePercentage}`);
   return watcherStruct.disk;
-}
+};
 
 exports.getAllStats = async () => {
   await this.getFreeCPU();
   await this.getFreeMem();
   await this.getFreeDiskSpace();
   return watcherStruct;
-}
+};
